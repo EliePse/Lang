6c6046a982540d1b2ad18c8c4e8fea2a900ef1bc
Elie Meignan <eliepse.meignan@gmail.com>
2016-09-22 13:19:28 +0200

    Draft of config file

---
+++ config/lang.php
@@ -0,0 +1,21 @@
+<?php
+
+return [
+
+	"cookie-expire" => 2592000,
+	"cookie-path" => "/",
+	"cookie-domain" => null,
+
+	"default-local" => "fr",
+
+	"local-enabled" => [
+		"fr" => "fr"
+	],
+
+	"source-folder" => "resources/lang/",
+
+	"local-force-file" => [],
+
+//	"local-blacklist" => [],
+
+];
